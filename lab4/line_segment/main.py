25086ae5409236a4d798b1baeb95376df9056229
Alexander <romanovAlx@gmail.com>
2025-03-17 17:19:54 +0300

    Добавил main.py

---
+++ lab4/line_segment/main.py
@@ -0,0 +1,12 @@
+# main.py - тестирование класса
+
+segment = LineSegment(0, 0, 3, 4)
+print(segment)  # LineSegment((0, 0) - (3, 4))
+print("Длина:", segment.length())  # 5.0
+    
+segment.save("segment.json")
+loaded_segment = LineSegment.load("segment.json")
+print("Загруженный:", loaded_segment)
+    
+new_segment = LineSegment.from_string("1,2,4,6")
+print("Из строки:", new_segment)
