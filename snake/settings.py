0063c66760f3d6eb98ef4abec1a05818ae580a98
Alexander <romanovAlx@gmail.com>
2025-04-08 22:19:15 +0300

    settings

---
+++ snake/settings.py
@@ -0,0 +1,17 @@
+# Настройки игры
+WIDTH, HEIGHT = 640, 480
+CELL_SIZE = 20
+FPS_START = 10
+
+# Цвета
+BLACK = (0, 0, 0)
+GREEN = (0, 200, 0)
+RED = (255, 0, 0)
+
+# Направления
+DIRECTIONS = {
+    'UP': (0, -1),
+    'DOWN': (0, 1),
+    'LEFT': (-1, 0),
+    'RIGHT': (1, 0)
+}
