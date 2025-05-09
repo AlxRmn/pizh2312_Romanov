95185106d7ad27ff5b351a82a2e1b880b5d694f7
Alexander <romanovAlx@gmail.com>
2025-05-09 16:19:25 +0300

    create class Client

---
+++ LR1 C++/LR1 C++.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Client {
+private:
+    string name;
+    string address;
+    string contractNumber;
+
+public:
+    Client() : name(""), address(""), contractNumber("") {}
+
+    void setClient(string n, string a, string c) {
+        name = n;
+        address = a;
+        contractNumber = c;
+    }
+
+    void setClient(string n, string a) {
+        name = n;
+        address = a;
+        contractNumber = "Не указан";
+    }
+
+    void setClient(string n) {
+        name = n;
+        address = "Не указан";
+        contractNumber = "Не указан";
+    }
+
+    void displayClient() const {
+        cout << "Имя: " << name << ", Адрес: " << address << ", Номер договора: " << contractNumber << endl;
+    }
+};
