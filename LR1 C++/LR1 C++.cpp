2107afd3e6f4595f6215f10ce18181b06d5d8f28
Alexander <romanovAlx@gmail.com>
2025-05-09 16:26:23 +0300

    create class RegistrationJournal

--- LR1 C++/LR1 C++.cpp
+++ LR1 C++/LR1 C++.cpp
@@ -33,4 +33,53 @@ public:
     void displayClient() const {
         cout << "Имя: " << name << ", Адрес: " << address << ", Номер договора: " << contractNumber << endl;
     }
+}; class RegistrationJournal {
+private:
+    string organizationName;
+    string phoneNumber;
+    vector<Client> clients; 
+
+public:
+   
+    void setOrganizationInfo(string name, string phone) {
+        organizationName = name;
+        phoneNumber = phone;
+    }
+
+    void addClient(const Client& client) {
+        clients.push_back(client);
+    }
+
+    void displayJournal() const {
+        cout << "Организация: " << organizationName << endl;
+        cout << "Телефон: " << phoneNumber << endl;
+        cout << "Клиенты:" << endl;
+        for (const auto& client : clients) {
+            client.displayClient();
+        }
+    }
 };
+
+
+int main() {
+
+    RegistrationJournal journal;
+    journal.setOrganizationInfo("Компания Рога и Копыта", "+7-123-456-7890");
+
+    Client client1;
+    client1.setClient("Иван Иванов", "г. Москва", "Договор №123");
+
+    Client client2;
+    client2.setClient("Петр Петров", "г. Санкт-Петербург");
+
+    Client client3;
+    client3.setClient("Сидор Сидоров");
+
+    journal.addClient(client1);
+    journal.addClient(client2);
+    journal.addClient(client3);
+
+    journal.displayJournal();
+
+    return 0;
+}
