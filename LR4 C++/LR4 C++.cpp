51fc7034c7fb2a4bcde1b2ae26e93e4be1024cf0
Alexander <romanovAlx@gmail.com>
2025-05-13 19:46:41 +0300

    Create abstract Client

---
+++ LR4 C++/LR4 C++.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// Абстрактный базовый класс Client
+class Client {
+protected:
+    string name;
+    string address;
+    string contractNumber;
+
+public:
+    Client() : name(""), address(""), contractNumber("") {
+        cout << "[Client] Конструктор по умолчанию\n";
+    }
+
+    Client(string n, string a, string c) : name(n), address(a), contractNumber(c) {
+        cout << "[Client] Конструктор с параметрами\n";
+    }
+
+    virtual ~Client() {
+        cout << "[Client] Деструктор\n";
+    }
+
+    virtual void displayClient() const = 0;
+};
